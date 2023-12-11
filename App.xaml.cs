af0e1bc0c6ba41cd61cc5ca36abd85f62fe5db21
DiaconuAndreea <148005437+DiaconuAndreea@users.noreply.github.com>
2023-12-12 00:36:52 +0200

    Add files via upload

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,24 @@
-﻿namespace Diaconu_Andreea_lab7
+﻿using System;
+using Diaconu_Andreea_lab7.Data;
+using System.IO;
+namespace Diaconu_Andreea_lab7
 {
     public partial class App : Application
     {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new
+                   ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                   LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
         public App()
         {
             InitializeComponent();
